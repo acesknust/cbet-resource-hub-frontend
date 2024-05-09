d74aad6b7d722e0f2a7b8f583a938d66458b9023
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2024-05-09 22:51:36 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="content">
+          <h2>CBET</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            sed, officia quae eum iusto ratione autem eius minus, nisi
+            laboriosam id ullam aliquam sunt deserunt, magni natus maiores
+            placeat perferendis.
+          </p>
+          <div className="logos">
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+          </div>
+          <div  className="buttons">
+          <button >Sign-Up</button>
+          <button className="login-button">Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
