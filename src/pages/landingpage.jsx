4be178d08d2a2a2648c8e5d199777b6a86bc58e5
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2024-05-28 21:47:13 +0000

    created a page for the landingpage

---
+++ src/pages/landingpage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const landingpage = () => {
+  return (
+    <div>
+      <div className="App">
+      <div className="container">
+        <div className="content">
+          <h2>CBET</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            sed, officia quae eum iusto ratione autem eius minus, nisi
+            laboriosam id ullam aliquam sunt deserunt, magni natus maiores
+            placeat perferendis.
+          </p>
+          <div className="logos">
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+          </div>
+          <div className="buttons">
+            <button>Sign-Up</button>
+            <button className="login-button">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default landingpage
