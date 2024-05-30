e062dafd2c089ceecba6a7b48c2cb48e17d5c7d5
anamboijohn <anamboijohn@gmail.com>
2024-05-30 21:07:03 +0000

    fix merge conflicts

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -5,9 +5,6 @@ const Landingpage = () => {
   return (
     <div>
       <div className="App">
-        <Link to="/Home" style={{ textDecoration: "None", color: "#fff" }}>
-          Home
-        </Link>
         <div className="container">
           <div className="content">
             <h2 style={{color: '#fff'}}>CBET</h2>
@@ -26,24 +23,32 @@ const Landingpage = () => {
             </div>
             <div className="buttons">
 
-              <button className="login-button"><Link to="/SignUp" style={{textDecoration: 'None', color: '#fff'}}>Sign Up</Link></button>
+              {/* <button className="login-button"><Link to="/SignUp" style={{textDecoration: 'None', color: '#fff'}}>Sign Up</Link></button>
               <button className="login-button"><Link to="/SignIn" style={{textDecoration: 'None', color: '#fff'}}>Login</Link></button>
-              <button className="login-button"><Link to="/Home" style={{textDecoration: 'None', color: '#fff'}}>Home</Link></button>
+              <button className="login-button"><Link to="/Home" style={{textDecoration: 'None', color: '#fff'}}>Home</Link></button> */}
 
-//               <Link
-//                 to="/SignUp"
-//                 style={{ textDecoration: "None", textAlign:"center"}}
-//               >
-//                 <button style={{ background:"inherit", border:"none", color:"white" }}>Sign Up</button>
-//               </Link>
+               <Link
+                 to="/SignUp"
+                 style={{ textDecoration: "None", textAlign:"center"}}
+               >
+                 <button style={{ background:"inherit", border:"none", color:"white" }}>Sign Up</button>
+               </Link>
 
-//               <Link
-//                 to="/SignIn"
-//                 style={{ textDecoration: "None", textAlign:"center" }}
-//                 className="login-button"
-//               >
-//                 <button  style={{ background:"inherit", border:"none", color:"white" }}>Login</button>
-//               </Link>
+              <Link
+                 to="/SignIn"
+                style={{ textDecoration: "None", textAlign:"center" }}
+                className="login-button"
+               >
+                <button  style={{ background:"inherit", border:"none", color:"white" }}>Login</button>
+               </Link>
+          
+              <Link
+                 to="/Home"
+                style={{ textDecoration: "None", textAlign:"center" }}
+                className="login-button"
+               >
+                <button  style={{ background:"inherit", border:"none", color:"white" }}>Home</button>
+               </Link>
 
             </div>
           </div>
