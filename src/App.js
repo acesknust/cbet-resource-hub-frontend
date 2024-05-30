7cb116505918350a4fb2dd7e0d449a7af366d0ab
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2024-05-30 22:49:54 +0000

    added course files route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Landingpage from "./pages/landingpage";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import CourseFiles from "./pages/CourseFiles";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landingpage />} />
-
+        
         <Route path="/Home" element={<Home />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/CourseFiles" element={<CourseFiles />} />
       </Routes>
     </Router>
   );
